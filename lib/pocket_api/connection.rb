0d4669c65201c11755b3f9c3cdbf1480516652c6
Alex Chee <alexchee11@gmail.com>
2014-02-24 18:59:45 -0800

    fixed headers not passing in to server

--- lib/pocket_api/connection.rb
+++ lib/pocket_api/connection.rb
@@ -17,7 +17,7 @@ module PocketApi
       # 3. Do a sample call to API to see if it works
       #####################################################
       def generate_request_token(body_options={}, headers_options={})
-        response = post("/v3/oauth/request", {:body => MultiJson.dump({:consumer_key => @client_key}.merge(body_options)), :headers => {"Content-Type" => "application/json; charset=UTF-8", "X-Accept" => "application/json"}.merge(headers_options)})
+        response = post("/v3/oauth/request", :body => MultiJson.dump({:consumer_key => @client_key}.merge(body_options)), :headers => {"Content-Type" => "application/json; charset=UTF-8", "X-Accept" => "application/json"}.merge(headers_options))
         if response.success? && response.parsed_response["code"]
           # Should be a string body like "code=12345678"
           @request_token = response.parsed_response["code"]
@@ -27,7 +27,7 @@ module PocketApi
       end
       
       def generate_access_token(request_token)
-        response = post("/auth/authorize", {:body => MultiJson.dump({:code => @request_token, :consumer_key => @client_key}), :headers => {"Content-Type" => "application/json; charset=UTF-8", "X-Accept" => "application/json"}})
+        response = post("/auth/authorize", :body => MultiJson.dump({:code => (request_token || @request_token), :consumer_key => @client_key}), :headers => {"Content-Type" => "application/json; charset=UTF-8", "X-Accept" => "application/json"})
         response.parsed_response["access_token"]
       end
       
